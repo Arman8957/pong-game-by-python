7cd80d5aa70acf406332d520ba6efa5ab45a0cde
git learning <mdarmanya.h@gmail.com>
2023-02-27 06:20:17 -0800

    pong game

---
+++ atmMachine.py
@@ -0,0 +1,23 @@
+# class AtmMachine:
+#     def __init__(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#
+#
+#
+#
+#     def machine(self):
+#         value = self.num2
+#
+#         i = value
+#         print(i)
+#     def getPass(self):
+#         print(self.num1)
+
+
+
+
+
+
+
+
